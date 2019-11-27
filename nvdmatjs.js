c449813a695b1c0baf425ba03f1bcaedd7b58415
dli <terryinzaghi@163.com>
2019-11-27 03:20:01 +0000

    docs

--- nvdmatjs.js
+++ nvdmatjs.js
@@ -1,6 +1,10 @@
 const ndfunc = require("./ndfunc.js")
 const ding = require("./plugin/ding.js")
+const cmmn = require("./cmmn-utils.js")
+
 
 module.exports = {
     ndfunc : ndfunc,
+    ding : ding,
+    cmmn : cmmn
 }
